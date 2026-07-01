be8861f3c57aa1d976a1439710c39f02df1312cc
Steel <oldpapasteel@gmail.com>
2026-07-01 16:33:05 -0600

    Update dimensional_death.lua
    Slight damage increase for a scarily-named ability.

--- scripts/actions/mobskills/dimensional_death.lua
+++ scripts/actions/mobskills/dimensional_death.lua
@@ -15,7 +15,7 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
 
     params.baseDamage     = mob:getWeaponDmg()
     params.numHits        = 1
-    params.fTP            = { 2.25, 2.25, 2.25 }
+    params.fTP            = { 2.5, 2.5, 2.5 }
     params.attackType     = xi.attackType.PHYSICAL
     params.damageType     = xi.damageType.HTH
     params.shadowBehavior = xi.mobskills.shadowBehavior.NUMSHADOWS_1
