3bc0efdaa756953db8b872c3db43b460cca4c386
Steel <oldpapasteel@gmail.com>
2026-07-01 16:03:10 -0600

    Update ultrasonics.lua
    Reduced duration.,

--- scripts/actions/mobskills/ultrasonics.lua
+++ scripts/actions/mobskills/ultrasonics.lua
@@ -11,7 +11,7 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
 end
 
 mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
-    skill:setMsg(xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.EVASION_DOWN, 25, 0, 180))
+    skill:setMsg(xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.EVASION_DOWN, 30, 0, 90))
 
     return xi.effect.EVASION_DOWN
 end
