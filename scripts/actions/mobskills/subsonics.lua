52db2bb5849106581d1c86e3b855363cdf4c57a2
Steel <oldpapasteel@gmail.com>
2026-07-01 16:03:40 -0600

    Update subsonics.lua
    Reduced duration.

--- scripts/actions/mobskills/subsonics.lua
+++ scripts/actions/mobskills/subsonics.lua
@@ -11,7 +11,7 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
 end
 
 mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
-    skill:setMsg(xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.DEFENSE_DOWN, 25, 0, 180))
+    skill:setMsg(xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.DEFENSE_DOWN, 30, 0, 90))
 
     return xi.effect.DEFENSE_DOWN
 end
