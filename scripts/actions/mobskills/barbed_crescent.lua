b7763e813fb2e47b234f787d7b2b6cbdc136b07a
Steel <oldpapasteel@gmail.com>
2026-07-01 16:29:30 -0600

    Update barbed_crescent.lua
    Reduced duration.

--- scripts/actions/mobskills/barbed_crescent.lua
+++ scripts/actions/mobskills/barbed_crescent.lua
@@ -26,7 +26,7 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
     if xi.mobskills.processDamage(mob, target, skill, action, info) then
         target:takeDamage(info.damage, mob, info.attackType, info.damageType)
 
-        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.ACCURACY_DOWN, 100, 0, 180)
+        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.ACCURACY_DOWN, 100, 0, 120)
     end
 
     return info.damage
