412d1090602562b16b99d6392d4b0316e0ad2218
Steel <oldpapasteel@gmail.com>
2026-07-01 16:32:27 -0600

    Update ectosmash.lua
    Slight damage increase.

--- scripts/actions/mobskills/ectosmash.lua
+++ scripts/actions/mobskills/ectosmash.lua
@@ -17,7 +17,7 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
 
     params.baseDamage       = mob:getWeaponDmg()
     params.numHits          = 1
-    params.fTP              = { 2.0, 2.0, 2.0 }
+    params.fTP              = { 2.2, 2.2, 2.2 }
     params.attackType       = xi.attackType.PHYSICAL
     params.damageType       = xi.damageType.BLUNT
     params.shadowBehavior   = xi.mobskills.shadowBehavior.NUMSHADOWS_1
