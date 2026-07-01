cecc6a812e15f1e065eabee3b6ed44a6b0bd2af2
Steel <oldpapasteel@gmail.com>
2026-07-01 15:17:28 -0600

    Update spiral_spin.lua
    Increased duration.

--- scripts/actions/mobskills/spiral_spin.lua
+++ scripts/actions/mobskills/spiral_spin.lua
@@ -27,7 +27,7 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
     if xi.mobskills.processDamage(mob, target, skill, action, info) then
         target:takeDamage(info.damage, mob, info.attackType, info.damageType)
 
-        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.ACCURACY_DOWN, 40, 0, 60)
+        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.ACCURACY_DOWN, 50, 0, 90)
     end
 
     return info.damage
