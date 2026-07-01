40b1647adbb5fd8abb429ac45348444ae3c50cff
Steel <oldpapasteel@gmail.com>
2026-07-01 15:54:15 -0600

    Update fluid_spread.lua
    Added estimated fTPs instead of blank placeholders.

--- scripts/actions/mobskills/fluid_spread.lua
+++ scripts/actions/mobskills/fluid_spread.lua
@@ -16,7 +16,7 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
 
     params.baseDamage       = mob:getWeaponDmg()
     params.numHits          = 1
-    params.fTP              = { 1.0, 1.0, 1.0 } -- TODO: Capture fTPs
+    params.fTP              = { 1.0, 1.25, 1.5 } -- TODO: Capture fTPs
     params.attackType       = xi.attackType.PHYSICAL
     params.damageType       = xi.damageType.SLASHING
     params.shadowBehavior   = xi.mobskills.shadowBehavior.NUMSHADOWS_3 -- TODO: Capture shadowBehavior
