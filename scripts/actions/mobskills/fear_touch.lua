1f80a75c0e2d0b5e9f9228f91388aa44109077f1
Steel <oldpapasteel@gmail.com>
2026-07-01 16:46:35 -0600

    Update fear_touch.lua
    NORMALLY supposed to inflict a 5-minute slow, which was missing from LSB code. Now inflicts a 15s bind instead.

--- scripts/actions/mobskills/fear_touch.lua
+++ scripts/actions/mobskills/fear_touch.lua
@@ -1,7 +1,7 @@
 -----------------------------------
 -- Fear Touch
 -- Family: Ghost
--- Description: Deals physical damage to a single target. Additional Effect : Slow
+-- Description: Deals physical damage to a single target. Additional Effect : Bind
 -----------------------------------
 ---@type TMobSkill
 local mobskillObject = {}
@@ -15,17 +15,19 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
 
     params.baseDamage     = mob:getWeaponDmg()
     params.numHits        = 1
-    params.fTP            = { 1.0, 1.0, 1.0 }
+    params.fTP            = { 1.25, 1.25, 1.25 }
     params.attackType     = xi.attackType.PHYSICAL
     params.damageType     = xi.damageType.SLASHING
     params.shadowBehavior = xi.mobskills.shadowBehavior.NUMSHADOWS_1
     params.canCrit        = true
-    params.criticalChance = { 0.10, 0.20, 0.25 } -- TODO: Capture crit rate
+    params.criticalChance = { 0.10, 0.20, 0.25 }
 
     local info = xi.mobskills.mobPhysicalMove(mob, target, skill, action, params)
 
     if xi.mobskills.processDamage(mob, target, skill, action, info) then
         target:takeDamage(info.damage, mob, info.attackType, info.damageType)
+
+        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.BIND, 1, 0, 15)
     end
 
     return info.damage
