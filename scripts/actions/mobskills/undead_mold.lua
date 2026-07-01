c39f71eefe197a287a52ec9abf72ecf068ceba3a
Steel <oldpapasteel@gmail.com>
2026-07-01 16:27:28 -0600

    Update undead_mold.lua
    I'm not really changing anything here, just pointing out that 11m disease is a silly number.

--- scripts/actions/mobskills/undead_mold.lua
+++ scripts/actions/mobskills/undead_mold.lua
@@ -27,7 +27,7 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
     if xi.mobskills.processDamage(mob, target, skill, action, info) then
         target:takeDamage(info.damage, mob, info.attackType, info.damageType)
 
-        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.DISEASE, 1, 0, 660)
+        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.DISEASE, 1, 0, 600)
     end
 
     return info.damage
