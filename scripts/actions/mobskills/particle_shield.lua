c40e30509470637db6ebd981484b7e0986998377
Steel <oldpapasteel@gmail.com>
2026-07-01 20:09:34 -0600

    Update particle_shield.lua
    Increased effect, reduced duration.

--- scripts/actions/mobskills/particle_shield.lua
+++ scripts/actions/mobskills/particle_shield.lua
@@ -13,7 +13,7 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
 end
 
 mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
-    skill:setMsg(xi.mobskills.mobBuffMove(mob, xi.effect.DEFENSE_BOOST, 50, 0, 300))
+    skill:setMsg(xi.mobskills.mobBuffMove(mob, xi.effect.DEFENSE_BOOST, 100, 0, 120))
 
     return xi.effect.DEFENSE_BOOST
 end
