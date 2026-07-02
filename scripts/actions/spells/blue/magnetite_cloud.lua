a999f921f12409d4c9759abdd58df43d7eda3145
Steel <oldpapasteel@gmail.com>
2026-07-02 10:16:34 -0600

    Update magnetite_cloud.lua
    Reduced weight duration so BLU can't perma-kite, increased effectiveness slightly.

--- scripts/actions/spells/blue/magnetite_cloud.lua
+++ scripts/actions/spells/blue/magnetite_cloud.lua
@@ -1,13 +1,13 @@
 -----------------------------------
 -- Spell: Magnetite Cloud
 -- Deals earth damage to enemies within a fan-shaped area originating from the caster. Additional effect: Weight
--- Spell cost: 86 MP
+-- Spell cost: 90 MP
 -- Monster Type: Beastmen
 -- Spell Type: Magical (Earth)
 -- Blue Magic Points: 3
 -- Stat Bonus: VIT+2
 -- Level: 46
--- Casting Time: 4.5 seconds
+-- Casting Time: 3 seconds
 -- Recast Time: 29.25 seconds
 -- Magic Bursts on: Scission, Gravitation, and Darkness
 -- Combos: Magic Defense Bonus
@@ -27,7 +27,7 @@ spellObject.onSpellCast = function(caster, target, spell)
     params.diff       = 0 -- no stat increases magic accuracy
     params.skillType  = xi.skill.BLUE_MAGIC
     params.hpMod      = 6
-    params.lvlMod     = 1.875
+    params.lvlMod     = 1
     params.isConal    = true
 
     -- Handle damage.
@@ -40,7 +40,7 @@ spellObject.onSpellCast = function(caster, target, spell)
     -- Handle status effects.
     local effectTable =
     {
-        [1] = { xi.effect.WEIGHT, 25, 0, 60 },
+        [1] = { xi.effect.WEIGHT, 35, 0, 30 },
     }
 
     xi.spells.blue.applyBlueAdditionalEffect(caster, target, params, effectTable)
