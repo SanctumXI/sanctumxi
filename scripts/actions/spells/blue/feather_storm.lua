5ff8bab16385d90d51d4a681650926e74eb98766
Steel <oldpapasteel@gmail.com>
2026-07-02 12:53:40 -0600

    Update feather_storm.lua

--- scripts/actions/spells/blue/feather_storm.lua
+++ scripts/actions/spells/blue/feather_storm.lua
@@ -21,31 +21,28 @@ end
 
 spellObject.onSpellCast = function(caster, target, spell)
     local params = {}
+
     params.ecosystem = xi.ecosystem.BEASTMEN
-    params.tpmod = xi.spells.blue.tpMod.CRITICAL
-    params.critchance = 0
-    if caster:hasStatusEffect(xi.effect.AZURE_LORE) then
-        params.critchance = 55
-    elseif caster:hasStatusEffect(xi.effect.CHAIN_AFFINITY) then
-        params.critchance = math.floor(caster:getTP() / 75)
-    end
+    params.tpmod     = xi.spells.blue.tpMod.CRITICAL
 
     params.attackType = xi.attackType.RANGED
     params.damageType = xi.damageType.PIERCING
     params.scattr     = xi.skillchainType.TRANSFIXION
+
     params.numhits    = 1
     params.multiplier = 2
     params.tp150      = 2
     params.tp300      = 2
     params.azuretp    = 2
     params.duppercap  = 17
-    params.str_wsc    = 0.0
-    params.dex_wsc    = 0.0
-    params.vit_wsc    = 0.0
-    params.agi_wsc    = 0.3
-    params.int_wsc    = 0.0
-    params.mnd_wsc    = 0.0
-    params.chr_wsc    = 0.0
+
+    params.str_wsc = 0.0
+    params.dex_wsc = 0.3
+    params.vit_wsc = 0.0
+    params.agi_wsc = 0.0
+    params.int_wsc = 0.0
+    params.mnd_wsc = 0.0
+    params.chr_wsc = 0.0
 
     -- Handle damage.
     local damage = xi.spells.blue.usePhysicalSpell(caster, target, spell, params)
