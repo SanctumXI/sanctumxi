9dfc6843bec87b933e82c0f7a7b337ae96b3c7b1
Steel <oldpapasteel@gmail.com>
2026-07-02 10:28:32 -0600

    Update bad_breath.lua
    Consolidated changes. Reduced MP cost slightly for such an iconic spell, reduced status durations.

--- scripts/actions/spells/blue/bad_breath.lua
+++ scripts/actions/spells/blue/bad_breath.lua
@@ -1,14 +1,14 @@
 -----------------------------------
 -- Spell: Bad Breath
 -- Deals earth damage that inflicts multiple status ailments on enemies within a fan-shaped area originating from the caster
--- Spell cost: 212 MP
+-- Spell cost: 170 MP
 -- Monster Type: Plantoids
 -- Spell Type: Magical (Earth)
 -- Blue Magic Points: 5
 -- Stat Bonus: INT+2, MND+2
 -- Level: 61
--- Casting Time: 8.75 seconds
--- Recast Time: 120 seconds
+-- Casting Time: 3 seconds
+-- Recast Time: 90 seconds
 -- Magic Bursts on: Scission, Gravitation, Darkness
 -- Combos: Fast Cast
 -----------------------------------
@@ -27,7 +27,7 @@ spellObject.onSpellCast = function(caster, target, spell)
     params.diff       = 0 -- no stat increases magic accuracy
     params.skillType  = xi.skill.BLUE_MAGIC
     params.hpMod      = 8
-    params.lvlMod     = 3
+    params.lvlMod     = 1
     params.isConal    = true
 
     -- Handle damage.
@@ -40,13 +40,13 @@ spellObject.onSpellCast = function(caster, target, spell)
     -- Handle status effects.
     local effectTable =
     {
-        [1] = { xi.effect.SLOW,      2000, 0, 60 },
-        [2] = { xi.effect.SILENCE,      1, 0, 60 },
-        [3] = { xi.effect.PARALYSIS,   15, 0, 60 },
-        [4] = { xi.effect.BIND,         1, 0, 60 },
-        [5] = { xi.effect.WEIGHT,      20, 0, 60 },
-        [6] = { xi.effect.POISON,       4, 0, 60 },
-        [7] = { xi.effect.BLINDNESS,   20, 0, 60 },
+        [1] = { xi.effect.SLOW,      2000, 0, 30 },
+        [2] = { xi.effect.SILENCE,      1, 0, 30 },
+        [3] = { xi.effect.PARALYSIS,   15, 0, 30 },
+        [4] = { xi.effect.BIND,         1, 0, 30 },
+        [5] = { xi.effect.WEIGHT,      20, 0, 30 },
+        [6] = { xi.effect.POISON,       7, 0, 30 },
+        [7] = { xi.effect.BLINDNESS,   20, 0, 30 },
     }
 
     xi.spells.blue.applyBlueAdditionalEffect(caster, target, params, effectTable)
