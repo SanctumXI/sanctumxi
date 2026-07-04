c92a18d6054ed7d379c17962473dde95c407ed7d
Xaver-DaRed <xaver.entropia@gmail.com>
2026-07-04 15:01:23 +0200

    Implement ecosystem damage multipliers
    Circle effects. Blue spell ecosystem properties, etc...

--- scripts/effects/arcane_circle.lua
+++ scripts/effects/arcane_circle.lua
@@ -5,15 +5,18 @@
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
-        local darkSkill = target:getSkillLevel(xi.skill.DARK_MAGIC)
+    local darkSkill = target:getSkillLevel(xi.skill.DARK_MAGIC)
     local power = math.floor((darkSkill / 20) + 5)
+    local jpValue = target:getJobPointLevel(xi.jp.ARCANE_CIRCLE_EFFECT) -- Only affects damage received.
 
-       effect:addMod(xi.mod.ARCANA_KILLER, effect:getPower())
+    effect:addMod(xi.mod.ARCANA_KILLER, effect:getPower())
+    effect:addMod(xi.mod.ARCANA_DMG_MULTIPLIER, effect:getPower())
+    effect:addMod(xi.mod.ARCANA_RES_MULTIPLIER, effect:getPower() + jpValue)
+
     -- Sanctum Custom effect (Disabled)
     -- effect:addMod(xi.mod.ENSPELL, xi.element.DARK)
     -- effect:addMod(xi.mod.ENSPELL_DMG, power)
     -- effect:addMod(xi.mod.ATTP, 5)
- 
 end
 
 effectObject.onEffectTick = function(target, effect)
--- scripts/effects/holy_circle.lua
+++ scripts/effects/holy_circle.lua
@@ -7,8 +7,12 @@ local effectObject = {}
 effectObject.onEffectGain = function(target, effect)
     local divineSkill = target:getSkillLevel(xi.skill.DIVINE_MAGIC)
     local power = math.floor((divineSkill / 20) + 5)
+    local jpValue = target:getJobPointLevel(xi.jp.HOLY_CIRCLE_EFFECT) -- Only affects damage received.
 
     target:addMod(xi.mod.UNDEAD_KILLER, effect:getPower())
+    effect:addMod(xi.mod.UNDEAD_DMG_MULTIPLIER, effect:getPower())
+    effect:addMod(xi.mod.UNDEAD_RES_MULTIPLIER, effect:getPower() + jpValue)
+
     -- Sanctum Custom effect (Disabled)
     -- effect:addMod(xi.mod.ENSPELL, xi.element.LIGHT)
     -- effect:addMod(xi.mod.ENSPELL_DMG, power)
@@ -20,7 +24,6 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.UNDEAD_KILLER, effect:getPower())
-
 end
 
 return effectObject
