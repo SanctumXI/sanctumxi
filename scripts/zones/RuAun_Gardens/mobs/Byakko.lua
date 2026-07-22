d18b90dfa7252823078e8f0f4e9923f46bc4757d
Xaver-DaRed <xaver.entropia@gmail.com>
2026-07-22 09:38:30 +0200

    Audit Bastok quest trades

--- scripts/zones/RuAun_Gardens/mobs/Byakko.lua
+++ scripts/zones/RuAun_Gardens/mobs/Byakko.lua
@@ -18,7 +18,6 @@ entity.onMobInitialize = function(mob)
     mob:setMobMod(xi.mobMod.GIL_MIN, 18000)
     mob:setMobMod(xi.mobMod.GIL_MAX, 18000)
     mob:setMobMod(xi.mobMod.WEAPON_BONUS, 32)
-    mob:setMobMod(xi.mobMod.BASE_DAMAGE_MODIFIER, 25)
     mob:setMod(xi.mod.TRIPLE_ATTACK, 45)
     mob:setMod(xi.mod.DOUBLE_ATTACK, 10)
     mob:setMod(xi.mod.VIT, 43)
@@ -28,6 +27,7 @@ entity.onMobSpawn = function(mob)
     mob:setMod(xi.mod.ATT, 391) -- 440 total attack.
     mob:setMod(xi.mod.DEF, 345) -- 415 total defense.
     mob:setMod(xi.mod.SILENCE_RES_RANK, 10)
+    mob:setMobMod(xi.mobMod.BASE_DAMAGE_MODIFIER, 25)
 
     mob:messageText(mob, ID.text.SKY_GOD_OFFSET + 11) -- Spawn message
     GetNPCByID(ID.npc.PORTAL_OFFSET + 8):setAnimation(xi.anim.CLOSE_DOOR)
